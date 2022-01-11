0d765e5711a900a04272cd5f29a86c1bc0e0c037
Giorgio <giorgio.collova@gmail.com>
2022-01-11 19:52:06 +0100

    starting project

--- frank-website/src/Pages/Home/index.jsx
+++ frank-website/src/Pages/Home/index.jsx
@@ -1,13 +1,13 @@
 import Header from "../../Modules/Header"
 import Hero from "../../Modules/Hero"
-import Sidebar from '../../Modules/Sidebar'
+
 
 
 const Home=()=>{
     return(
         <>
             <Header/>
-            <Sidebar/>
+            
             <Hero/>
         </>
     )
